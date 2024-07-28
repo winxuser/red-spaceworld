b85d41ecee17d0b08d708927abc122b599f8e5ea
Derek Andersen <derekcandersen@gmail.com>
2024-07-28 10:34:18 -0400

    increase wild encounter slots from 10 to 12 for every map; adjust wild encounter probabilities

--- constants/pokemon_data_constants.asm
+++ constants/pokemon_data_constants.asm
@@ -94,7 +94,7 @@ DEF NUM_EVOS_IN_BUFFER EQU 3
 DEF NUM_GROWTH_RATES EQU const_value
 
 ; wild data (see data/wild/maps/*.asm)
-DEF NUM_WILDMONS EQU 10
+DEF NUM_WILDMONS EQU 12 ; dereknote: increased wild mon slots from 10 to 12
 DEF WILDDATA_LENGTH EQU 1 + NUM_WILDMONS * 2
 
 ; PP in box_struct (see macros/ram.asm)
