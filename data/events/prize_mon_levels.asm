2516ad9b7061506ae432807dbb27f0e4ccf6c3a3
Derek Andersen <derekcandersen@gmail.com>
2024-08-01 16:16:29 -0400

    merge version differences in game corner prize corner, modify pokemon selection

--- data/events/prize_mon_levels.asm
+++ data/events/prize_mon_levels.asm
@@ -1,19 +1,10 @@
 PrizeMonLevelDictionary:
-IF DEF(_RED)
-	db ABRA,      9
-	db CLEFAIRY,  8
-	db NIDORINA, 17
-
-	db DRATINI,  18
-	db SCYTHER,  25
-	db PORYGON,  26
-ENDC
-IF DEF(_BLUE)
+	; first prize pokemon window
 	db ABRA,      6
-	db CLEFAIRY, 12
-	db NIDORINO, 17
-
-	db PINSIR,   20
-	db DRATINI,  24
+	db CLEFAIRY,  8
+	db DRATINI,  18
+	; second prize pokemon window
+	db MR_MIME,  15
 	db PORYGON,  18
-ENDC
+	db EEVEE,    15	
+	
