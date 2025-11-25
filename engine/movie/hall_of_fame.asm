974fbf570ba5317e2d68ef69080084d30ec9b5e1
Rangi <35663410+Rangi42@users.noreply.github.com>
2025-11-25 17:53:29 -0500

    Avoid magic numbers for most `CopyData` calls (#542)

--- engine/movie/hall_of_fame.asm
+++ engine/movie/hall_of_fame.asm
@@ -196,7 +196,7 @@ HoFLoadPlayerPics:
 	call UncompressSpriteFromDE
 	ld hl, sSpriteBuffer1
 	ld de, sSpriteBuffer0
-	ld bc, $310
+	ld bc, 2 * SPRITEBUFFERSIZE
 	call CopyData
 	ld de, vFrontPic
 	call InterlaceMergeSpriteBuffers
