22bbfc93fa07627d3890c6f9bd602bc8ccc71b29
winxuser <jdownting31@gmail.com>
2026-06-21 14:33:59 +1000

    Free some space in the Home BANK

--- macros/farcall.asm
+++ macros/farcall.asm
@@ -8,13 +8,13 @@
 MACRO farcall
 	ld b, BANK(\1)
 	ld hl, \1
-	call Bankswitch
+	rst _Bankswitch
 ENDM
 
 MACRO callfar
 	ld hl, \1
 	ld b, BANK(\1)
-	call Bankswitch
+	rst _Bankswitch
 ENDM
 
 MACRO farjp
@@ -33,23 +33,19 @@ MACRO homecall
 	ldh a, [hLoadedROMBank]
 	push af
 	ld a, BANK(\1)
-	ldh [hLoadedROMBank], a
-	ld [rROMB], a
+	call SetRomBank
 	call \1
 	pop af
-	ldh [hLoadedROMBank], a
-	ld [rROMB], a
+	call SetRomBank
 ENDM
 
 MACRO homecall_sf ; homecall but save flags by popping into bc instead of af
 	ldh a, [hLoadedROMBank]
 	push af
 	ld a, BANK(\1)
-	ldh [hLoadedROMBank], a
-	ld [rROMB], a
+	call SetRomBank
 	call \1
 	pop bc
 	ld a, b
-	ldh [hLoadedROMBank], a
-	ld [rROMB], a
+	call SetRomBank
 ENDM
--- scripts/ViridianMart.asm
+++ scripts/ViridianMart.asm
@@ -97,3 +97,7 @@ ViridianMartYoungsterText:
 ViridianMartCooltrainerMText:
 	text_far _ViridianMartCooltrainerMText
 	text_end
+
+ViridianMartClerkText:
+	script_mart POKE_BALL, ANTIDOTE, PARLYZ_HEAL, BURN_HEAL
+
